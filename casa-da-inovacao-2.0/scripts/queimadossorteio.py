180f82dc20100a1bf18bf9e1ea790a1ccd9eaea8
Natan Cabral <102121764+Ntzim@users.noreply.github.com>
2024-11-12 00:23:34 -0300

    Update queimadossorteio.py

--- casa-da-inovacao-2.0/scripts/queimadossorteio.py
+++ casa-da-inovacao-2.0/scripts/queimadossorteio.py
@@ -81,7 +81,7 @@ def baixar_excel(df, filename):
 
 # Configuração da aplicação
 st.title("Sorteio Edital | Casa da Inovação")
-st.image('')
+st.image('casa-da-inovacao-2.0/imagens/ID_CASA_INOVACAO -2.png')
 
 # Seletores de curso
 curso_selecionado = st.selectbox("Selecione o curso", [
